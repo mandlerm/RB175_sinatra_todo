108f9051817a4231e900691fe73105a12dfedbc9
mandlerm <amaya.melissa@gmail.com>
2019-10-18 21:23:12 -0500

    ability to delete a todo working

--- todo.rb
+++ todo.rb
@@ -23,9 +23,15 @@ get "/lists" do
   erb :lists, layout: :layout
 end
 
+# Render new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# Display a single ToDo
 get "/lists/:id" do
   id = params[:id].to_i
-  @list = session[:lists].fetch(id) if session[:lists].fetch(id)
+  @list = session[:lists].fetch(id)
   erb :list, layout: :layout
 end
 
@@ -34,6 +40,37 @@ get "/lists/:id/edit" do
   id = params[:id].to_i
   @list = session[:lists].fetch(id)
   erb :edit_list, layout: :layout
+end
+
+# Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+# Delete a todo list
+post '/lists/:id/destroy' do
+  id = params[:id].to_i
+  session[:lists].delete_at(id)
+  session[:success] = "The list has been deleted."
+  redirect "/lists"
+end
+
+# Add an item to a todo
+post '/lists/:id/todos' do
+  id = params[:id].to_i
+  @list = session[:lists].fetch(id)
+  @list[:todos] << params[:todo]
+  redirect "/lists/#{id}"
 end
 
 # Update an existing todo list
@@ -53,10 +90,6 @@ post "/lists/:id" do
   end
 end
 
-# Render new list form
-get "/lists/new" do
-  erb :new_list, layout: :layout
-end
 
 # Return an error message if name is invalid. Return nil if name is valid.
 def error_for_list_name(name)
@@ -64,20 +97,5 @@ def error_for_list_name(name)
     session[:error] = "List name must be between 1 and 100 characters"
   elsif session[:lists].any? { |list| list[:name] == name }
     session[:error] = "List name must be unique"
-  end
-end
-
-# Create a new list
-post "/lists" do
-  list_name = params[:list_name].strip
-
-  error = error_for_list_name(list_name)
-  if error
-    session[:error] = error
-    erb :new_list, layout: :layout
-  else
-    session[:lists] << { name: list_name, todos: [] }
-    session[:success] = "The list has been created."
-    redirect "/lists"
   end
 end
